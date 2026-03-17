934780f7be2f23ab9951bf8f01bba119fd112238
eijei? <62214702+eijeis@users.noreply.github.com>
2026-03-17 15:56:54 +0800

    Add files via upload

---
+++ Services/AttendanceImporter.cs
@@ -0,0 +1,45 @@
+﻿using BAMS.Repositories;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BAMS.Services
+{
+    public class AttendanceImporter
+    {
+        public void Import(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+
+            var grouped = lines
+                .Select(x => x.Split(','))
+                .Select(x => new
+                {
+                    UserId = int.Parse(x[0]),
+                    DateTime = DateTime.Parse(x[1])
+                })
+                .GroupBy(x => new { x.UserId, Day = x.DateTime.Date });
+
+            foreach (var g in grouped)
+            {
+                var times = g.Select(x => x.DateTime.TimeOfDay)
+                             .OrderBy(t => t)
+                             .ToList();
+
+                TimeSpan? amIn = null;
+                TimeSpan? amOut = null;
+                TimeSpan? pmIn = null;
+                TimeSpan? pmOut = null;
+
+                if (times.Count > 0) amIn = times[0];
+                if (times.Count > 1) amOut = times[1];
+                if (times.Count > 2) pmIn = times[2];
+                if (times.Count > 3) pmOut = times[3];
+
+                AttendanceRepository repo = new AttendanceRepository();
+                repo.InsertAttendance(g.Key.UserId, g.Key.Day, amIn, amOut, pmIn, pmOut);
+            }
+        }
+    }
+}
---
+++ Services/BiometricLogParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+public class BiometricLog
+{
+    public int UserId { get; set; }
+    public DateTime LogTime { get; set; }
+}
+
+public class BiometricLogParser
+{
+    public static List<BiometricLog> Parse(string path)
+    {
+        var logs = new List<BiometricLog>();
+        var lines = File.ReadAllLines(path);
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(' ');
+
+            if (parts.Length >=2)
+            {
+                logs.Add(new BiometricLog
+                {
+                    UserId = int.Parse(parts[0]),
+                    LogTime = DateTime.Parse(parts[1] + " " + parts[2])
+                });
+            }
+        }
+
+        return logs ;
+    }
+}
